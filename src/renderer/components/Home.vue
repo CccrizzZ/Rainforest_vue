bb2f23735db53b0b2c5f7d311a67cedf3d239c24
CccrizzZ <ccccrizzzz@gmail.com>
2019-12-25 19:51:56 -0500

    ##

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -28,27 +28,27 @@ export default {
     return{
       currtime: moment().format('MMMM Do YYYY, h:mm:ss a'),
       MyPlantsDB:[
-        {name: "OG kush", dominant: "Indica", CurrentTime: "Week 7"},
-        {name: "Blueberry", dominant: "Indica", CurrentTime: "Week 8"},
-        {name: "Jack Herer", dominant: "Sativa", CurrentTime: "Week 7"},
-        {name: "Blue Cheese", dominant: "Indica", CurrentTime: "Week 8"},
-        {name: "Blue Berry", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cheese", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream1", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream2", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream3", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream4", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream5", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream6", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream7", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream8", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream9", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream10", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream11", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream12", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream13", dominant: "Indica", CurrentTime: "Week 9"},
-        {name: "Blue Berry Cream14", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "OG kush", dominant: "Indica", CurrentTime: "Week 7"},
+        // {name: "Blueberry", dominant: "Indica", CurrentTime: "Week 8"},
+        // {name: "Jack Herer", dominant: "Sativa", CurrentTime: "Week 7"},
+        // {name: "Blue Cheese", dominant: "Indica", CurrentTime: "Week 8"},
+        // {name: "Blue Berry", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cheese", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream1", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream2", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream3", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream4", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream5", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream6", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream7", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream8", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream9", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream10", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream11", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream12", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream13", dominant: "Indica", CurrentTime: "Week 9"},
+        // {name: "Blue Berry Cream14", dominant: "Indica", CurrentTime: "Week 9"},
 
       ]
     }
@@ -68,7 +68,15 @@ export default {
         console.log("DB Creation Success: " + succ)
         console.log("DB Message: " + msg)
     })
-  
+    
+    if (db.valid("plantsDB", location)) {
+      db.getAll("plantsDB", location, (succ, data) => {
+        this.MyPlantsDB = data
+        console.log(data);
+        
+      })
+      
+    }
 
     // // if this DB exist at this specific location
     // if (db.valid("plantsDB", location)) {
--- src/renderer/components/reusable/PlantsCards.vue
+++ src/renderer/components/reusable/PlantsCards.vue
@@ -3,24 +3,24 @@
     <b-card header-tag="header" footer-tag="footer" v-for="plant in plants" v-bind:data="plant" v-bind:key="plant.name" style="color:#ffffff; background-color:#606060;">
       
       <!-- Card Title -->
-      <h6 slot="header" class="mb-0">{{plant.name}}</h6>
+      <h6 slot="header" class="mb-0">{{plant.Pname}}</h6>
       
       <!-- Card Subtitle -->
       <b-card-text>{{plant.dominant}}</b-card-text>
 
       <!-- Edit Button -->
-      <b-button variant="light" v-b-modal="plant.name">Edit</b-button>
+      <b-button variant="light" v-b-modal="plant.id">Edit</b-button>
       
       <!-- Editor Modal -->
-      <b-modal ref="EditModal" :id="plant.name" title="Edit Plant" hide-footer>
-        <h2 class="my-1">{{plant.name}}</h2>
+      <b-modal ref="EditModal" id="plant.id" title="Edit Plant" hide-footer>
+        <h2 class="my-1">{{plant.Pname}}</h2>
         <hr>
         <b-button variant="light" @click="hideModal">Cancel</b-button>
         <b-button variant="success" @click="saveModal">Save</b-button>
       </b-modal>
 
       <!-- Current Week -->
-      <em slot="footer">{{plant.CurrentTime}}</em>
+      <em slot="footer">{{plant.CurrWeek}}</em>
 
     </b-card>
   </b-card-group>
